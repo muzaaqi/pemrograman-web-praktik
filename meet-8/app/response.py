98b08698e96870ac611b2154608c0dd6e5d25b2c
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 08:24:09 +0700

    feat(res): Add response utility functions for Flask app
    Introduces ok and badRequest functions to standardize JSON responses with appropriate HTTP status codes in the Flask application.

---
+++ meet-8/app/response.py
@@ -0,0 +1,16 @@
+from flask import jsonify, make_response
+
+def ok(values, message):
+    res = {
+        'values': values,
+        'message': message,
+    }
+    
+    return make_response(jsonify(res), 200)
+
+def badRequest(message):
+    res = {
+        'message': message,
+    }
+    
+    return make_response(jsonify(res), 400)
