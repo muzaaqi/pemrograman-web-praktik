0a777f039554690efd7d1d9ab92f4e7ea89baee4
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 08:49:19 +0700

    feat(route): Add POST method to /users route
    The /users route now supports both GET and POST methods. GET requests return the user list, while POST requests create a new user using userController.store().

--- meet-8/app/routes.py
+++ meet-8/app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import request
 from app.controller import userController
 
 @app.route('/')
@@ -13,3 +14,10 @@ def users():
 @app.route('/users/<int:id>')
 def usesrDetail(id):
     return userController.show(id)
+
+@app.route('/users', methods=['POST', 'GET'])
+def users():
+    if request.method == 'GET':
+        return userController.index()
+    else:
+        return userController.store()
