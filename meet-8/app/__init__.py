103e7125a1740dd1a27f209210e15ea1e8618bc4
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 08:25:17 +0700

    chore(mod): Update imports in app/__init__.py
    Changed imports to explicitly import users and todo from app.model, and separated routes import. This improves clarity and modularity in the application initialization.

--- meet-8/app/__init__.py
+++ meet-8/app/__init__.py
@@ -5,7 +5,9 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
+from app.model import users, todo
+from app import routes
