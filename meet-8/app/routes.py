f6193dc49570027b64557b3cd3d6b9b7245b9778
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 08:58:36 +0700

    feat(route): Add PUT and DELETE methods to user detail route
    Expanded the '/users/<int:id>' route to support GET, PUT, and DELETE methods for retrieving, updating, and deleting user details. Also fixed a typo in the function name from 'usesrDetail' to 'userDetail'.

--- meet-8/app/routes.py
+++ meet-8/app/routes.py
@@ -11,9 +11,14 @@ def index():
 def users():
     return userController.index()
 
-@app.route('/users/<int:id>')
-def usesrDetail(id):
-    return userController.show(id)
+@app.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def userDetail(id):
+    if request.method == 'GET':
+        return userController.show(id)
+    elif request.method == 'PUT':
+        return userController.update(id)
+    elif request.method == 'DELETE':
+        return userController.delete(id)
 
 @app.route('/users', methods=['POST', 'GET'])
 def users():
