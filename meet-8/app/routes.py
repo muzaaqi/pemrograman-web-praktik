0ce6fba479ccb9a235efb194e092a850883f145d
MUZAAQI <muzaaqi@yahoo.com>
2025-12-04 08:26:29 +0700

    feat(route): Add /users route to app
    Introduced a new '/users' endpoint that calls userController.index(). This enhances the app by providing a dedicated route for user-related functionality.

--- meet-8/app/routes.py
+++ meet-8/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from app.controller import userController
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/users')
+def users():
+    return userController.index()
