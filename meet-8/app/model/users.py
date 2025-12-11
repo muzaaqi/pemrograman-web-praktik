400c0c438d39b60ab1053df663b61a68c489daee
MUZAAQI <muzaaqi@yahoo.com>
2025-12-11 22:13:56 +0700

    refactor: Increase password field length in Users model
    Changed the password column in the Users model from 128 to 255 characters to support longer password hashes or storage formats.

--- meet-8/app/model/users.py
+++ meet-8/app/model/users.py
@@ -7,7 +7,7 @@ class Users(db.Model):
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     name = db.Column(db.String(230), nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
     
