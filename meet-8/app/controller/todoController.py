e443bbd3ec8f76a214fbe72afe54552045d94690
MUZAAQI <muzaaqi@yahoo.com>
2025-12-11 07:51:36 +0700

    feat(controller): Add update method to todoController
    Introduces an update function to handle updating todo items by ID. The method retrieves the todo and description from the request, updates the corresponding fields, commits the changes to the database, and returns an appropriate response.

--- meet-8/app/controller/todoController.py
+++ meet-8/app/controller/todoController.py
@@ -28,3 +28,19 @@ def store():
     except Exception as e:
         print(e)
         return response.badRequest("Failed create new todo!")
+
+def update(id):
+    try:
+        todo = request.json.get('todo')
+        desc = request.json.get('description')
+        
+        todo = Todos.query.filter_by(id=id).first()
+        todo.todo = todo
+        todo.description = desc
+        
+        db.session.commit()
+        
+        return response.ok("", "Success update todo!")
+    except Exception as e:
+        print(e)
+        return response.badRequest("Failed update todo!")
